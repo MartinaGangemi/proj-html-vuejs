2d173baeffcfcf00a2252690bae9f21a4c44df48
MartinaGangemi <martinagangemi94@gmail.com>
2022-05-13 11:02:53 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <SiteHeader />
+    <SiteMain />
+    <SiteFooter />
+  </div>
+</template>
+
+<script>
+import SiteHeader from './components/HeaderComponent.vue'
+import SiteMain from './components/MainComponent.vue'
+import SiteFooter from './components/FooterComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+   SiteHeader,
+   SiteMain,
+   SiteFooter
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
