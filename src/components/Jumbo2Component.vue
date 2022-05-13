6a33cb78eba50475ffd603a8e1dd72d5be0017a7
MartinaGangemi <martinagangemi94@gmail.com>
2022-05-13 16:44:47 +0200

    Create Macro Area with block avada, health service, our doctor jumbo main component

---
+++ src/components/Jumbo2Component.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="jumbo">
+       
+    </div>
+</template>
+
+<script>
+export default{
+    name:"Jumbo2Component"
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
